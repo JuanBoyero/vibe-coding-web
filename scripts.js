c51094eba250f046273946f00739a826913c3848
Juan Boyero <114441425+JuanBoyero@users.noreply.github.com>
2025-04-04 19:28:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function playVideo(type) {
+    if (type === 'tralalero') {
+        window.location.href = "https://www.youtube.com/watch?v=R1PA-N6BCeM&pp=ygURdHJhbGFsZXJvIHRyYWxhbGE%3D" ;
+    } else if (type === 'rickroll') {
+        window.location.href = "https://www.youtube.com/watch?v=PyoRdu-i0AQ&pp=ygUMcmlja3JvbGxlYWRv";
+    }
+}
+
+// Aquí puedes agregar más funcionalidades si es necesario
